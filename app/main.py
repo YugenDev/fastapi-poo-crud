39134abf5e1a1f97a319bb85f53a1d3883622edb
yugoso8512 <yugoso8512@gmail.com>
2024-07-18 21:34:43 +0000

    Added route for sale

--- app/main.py
+++ app/main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI
 from crud.customer import router as customer_router
 from crud.employee import router as employee_router
 from crud.product import router as product_router
-#from crud.sale import router as sale_router
+from crud.sale import router as sale_router
 from crud.category import router as category_router
 from database.database import engine, Base
 from fastapi.middleware.cors import CORSMiddleware
@@ -23,5 +23,5 @@ app.add_middleware(
 app.include_router(customer_router, prefix="/customers", tags=["customers"])
 app.include_router(customer_router, prefix="/employees", tags=["employees"])
 app.include_router(product_router, prefix="/products", tags=["products"])
-#app.include_router(sale_router, prefix="/sales", tags=["sales"])
+app.include_router(sale_router, prefix="/sales", tags=["sales"])
 app.include_router(category_router, prefix="/categories", tags=["categories"])
