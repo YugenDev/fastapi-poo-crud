d77ca7147575b76ac7e56b491f163f285b17cbd7
yugoso8512 <yugoso8512@gmail.com>
2024-07-18 17:25:00 +0000

    Added delete_customer method

--- app/crud/customer.py
+++ app/crud/customer.py
@@ -11,5 +11,8 @@ def create_customer(db: Session, customer: schemas.CustomerCreate):
     db.refresh(db_customer)
     return db_customer
     
-# Pendiente: agregar los otros metodos
+def delete_customer(db: Session, customer_id: int):
+    db_customer = db.query(models.Customer).filter(models.Customer.costumer_id == customer_id).first()
+    db.delete(db_customer)
+    db.commit()
 
