02e3e16eb10dd2491e82a5477aad84c9c707e9fa
yugoso8512 <yugoso8512@gmail.com>
2024-07-18 18:46:49 +0000

    fixed imports and added dependecies

--- app/crud/customer.py
+++ app/crud/customer.py
@@ -1,8 +1,8 @@
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 from pydantic import BaseModel
-from database import SessionLocal
-from models import Customer as DBCustomer
+from database.database import SessionLocal
+from models.models import Customer as DBCustomer
 
 router = APIRouter()
 
--- app/models/models.py
+++ app/models/models.py
@@ -1,6 +1,6 @@
 from sqlalchemy import Column, Integer, String, Float, ForeignKey
 from sqlalchemy.orm import relationship
-from database import Base
+from database.database import Base
 
 class Customer(Base):
     __tablename__ = "customer"
