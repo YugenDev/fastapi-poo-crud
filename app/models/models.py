54bdc1829976a66c3fde282a606c652ae2dbb5ff
yugoso8512 <yugoso8512@gmail.com>
2024-07-18 19:42:08 +0000

    Added relations

--- app/models/models.py
+++ app/models/models.py
@@ -13,6 +13,9 @@ class Customer(Base):
     customer_type = Column(String(55), nullable=False)
     points = Column(Integer, nullable=False)
 
+    # Relación con ventas (uno a muchos)
+    sales = relationship("Sale", back_populates="customer")
+
 class Employee(Base):
     __tablename__ = "employee"
 
@@ -24,13 +27,21 @@ class Employee(Base):
     salary = Column(Float, nullable=False)
     position = Column(String(55), nullable=False)
 
+    # Relación con ventas (uno a muchos)
+    sales = relationship("Sale", back_populates="employee")
+
 class Category(Base):
     __tablename__ = 'category'
+
     category_id = Column(Integer, primary_key=True, autoincrement=True)
     category_name = Column(String(55), nullable=False)
 
+    # Relación con productos (uno a muchos)
+    products = relationship("Product", back_populates="category")
+
 class Product(Base):
     __tablename__ = 'product'
+
     product_id = Column(Integer, primary_key=True, autoincrement=True)
     product_name = Column(String(55), nullable=False)
     description = Column(String(250), nullable=False)
@@ -39,10 +50,15 @@ class Product(Base):
     quantity = Column(Integer, nullable=False)
     brand = Column(String(55), nullable=False)
 
+    # Relación con categoría (muchos a uno)
     category = relationship("Category", back_populates="products")
+
+    # Relación con ventas (uno a muchos)
+    sales = relationship("Sale", back_populates="product")
 
 class Sale(Base):
     __tablename__ = 'sales'
+
     sale_id = Column(Integer, primary_key=True, autoincrement=True)
     sale_date = Column(DateTime, nullable=False)
     customer_id = Column(Integer, ForeignKey('customer.customer_id'), nullable=False)
@@ -50,8 +66,13 @@ class Sale(Base):
     price = Column(Float, nullable=False)
     quantity = Column(Integer, nullable=False)
     total = Column(Float, nullable=False)
-    employee_id = Column(Integer, ForeignKey('employee.employee_id'), nullable=False)
 
-    product = relationship("Product", back_populates="sales")
+    # Relación con cliente (muchos a uno)
     customer = relationship("Customer", back_populates="sales")
+
+    # Relación con producto (muchos a uno)
+    product = relationship("Product", back_populates="sales")
+
+    # Relación con empleado (muchos a uno)
+    employee_id = Column(Integer, ForeignKey('employee.employee_id'), nullable=False)
     employee = relationship("Employee", back_populates="sales")
